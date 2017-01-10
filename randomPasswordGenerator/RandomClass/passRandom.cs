5efdf785d0e69091ff41460d2a4242c96430bcd5
VanHakobyan <van19962013@mail.ru>
2017-01-10 21:38:37 +0400

    edited 10.01

---
+++ randomPasswordGenerator/RandomClass/passRandom.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Text;
+
+namespace RandomClass
+{
+   static public class passRandom
+    {
+        static public string RanPass(byte length)
+        {
+            string inst = "0123456789abcdef";
+            //Hexadecimal symbols
+
+            Random Ran = new Random();
+
+            StringBuilder SB = new StringBuilder();
+
+            for (int i = 0; i < length; i++)
+            {
+                SB.Append(inst[Ran.Next(inst.Length)]);
+            }
+
+            return SB.ToString();
+        }
+    }
+}
---
+++ randomPasswordGenerator/TestingPassword/Program.cs
@@ -0,0 +1,20 @@
+﻿using RandomClass;
+using System;
+
+namespace TestingPassword
+{
+    class Program
+    {
+
+        static void Main()
+        {
+
+            string password = passRandom.RanPass(12);
+            //12-Length password
+            Console.WriteLine(password);
+
+
+            Console.ReadKey();
+        }
+    }
+}
